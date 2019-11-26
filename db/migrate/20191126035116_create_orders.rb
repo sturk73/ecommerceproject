a2d5f8a7cedfe4952a7ec3ae0e54e16990b85415
Sean Turk <seanturk1@gmail.com>
2019-11-25 21:51:44 -0600

    generate order model

---
+++ db/migrate/20191126035116_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.float :subtotal
+      t.float :provincal_tax
+      t.float :federal_tax
+      t.float :total
+      t.date :order_date
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
