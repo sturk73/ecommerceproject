9476c5bd05a398bd66e1ceb420089afd7126aadb
Sean Turk <seanturk1@gmail.com>
2019-12-04 12:49:12 -0600

    added category route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   scope '/api' do
     resources :products
+    resources :categories, only: %i[index]
   end
 end
