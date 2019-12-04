abeb456db0b82bdcf4972d0f3b3d1295b54bbc87
Sean Turk <seanturk1@gmail.com>
2019-12-04 12:27:04 -0600

    added category dropdown filter

--- client/src/imports/ui/index.jsx
+++ client/src/imports/ui/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Collapse,
   Navbar,
@@ -19,6 +19,7 @@ import CardImg from 'reactstrap/lib/CardImg';
 import CardBody from 'reactstrap/lib/CardBody';
 import CardText from 'reactstrap/lib/CardText';
 import CardSubtitle from 'reactstrap/lib/CardSubtitle';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 
 const divStyle = {
@@ -30,33 +31,86 @@ const img = {
   height: '300px',
 }
 
-class IndexPage extends Component{
-  constructor(){
+class IndexPage extends Component {
+  constructor() {
     super();
     this.state = {
+      dropdownOpen: false
     }
+
   }
 
   async componentDidMount() {
-    const response = await window.fetch('/api/products')
-    const data = await response.json()
+    const productResponse = await window.fetch('/api/products')
+    const products = await productResponse.json()
+
+    const categoriesResponse = await window.fetch('api/categories')
+    const categories = await categoriesResponse.json()
     this.setState({
-      data
+      products,
+      categories,
+      filteredProducts: products
     })
   }
 
-  onClickLinkHandler = (product)=> (e)=> {
+  onClickLinkHandler = (product) => (e) => {
     e.preventDefault();
     console.log(product)
-    this.props.history.push('/view-product', {product});
-}
+    this.props.history.push('/view-product', { product });
+  }
+
+  renderCategories() {
+
+
+    const toggle = () => {
+      this.setState({
+        dropdownOpen: !this.state.dropdownOpen
+      })
+    };
+
+    const categories = this.state.categories.map((category) => {
+      return (
+        <DropdownItem onClick={this.setCategory(category)}>{category.name}</DropdownItem>
+      )
+    })
+
+    return (
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>
+          Categories
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem onClick={this.setCategory("All")}>All</DropdownItem>
+          {categories}
+        </DropdownMenu>
+      </Dropdown>
+    )
+  }
+
+  setCategory = (category) => (e) => {
+    if (category === "All") {
+      this.setState({
+        selectedCategory: "All",
+        filteredProducts: this.state.products
+      })
+    }
+    else {
+      const products = this.state.products.filter((product) => { return product.category_id === category.id })
+
+      this.setState({
+        selectedCategory: category.name,
+        filteredProducts: products
+      })
+    }
+
+  }
 
   renderProducts() {
-    const product = this.state.data.map((product) => {
+    const product = this.state.filteredProducts.map((product) => {
       return (
         <div className="col-md-4" style={divStyle}>
           <Card className="col-md" style={divStyle}>
-            {product.image? <img style={img} src={product.image.url} /> : <img src="http://www.kovairealestateservices.com/uploads/thumbs/noimage.jpg" alt="No Image" />}
+            {product.image ? <img style={img} src={product.image.url} /> : <img src="http://www.kovairealestateservices.com/uploads/thumbs/noimage.jpg" alt="No Image" />}
             <CardBody>
               <CardTitle>{product.name}</CardTitle>
               <CardSubtitle>${product.product_price} + Tax</CardSubtitle>
@@ -66,26 +120,35 @@ class IndexPage extends Component{
               </div>
 
             </CardBody>
-        </Card>
-        </div>
-      )})
-
-      return (
-        <div className="row" style={divStyle}>
-          {product}
+          </Card>
         </div>
       )
+    })
+
+    return (
+      <div className="row" style={divStyle}>
+        {product}
+      </div>
+    )
   }
+
+  toggle = () => this.state.dropdownOpen(prevState => !prevState);
 
   render() {
 
-    if(!this.state.data){
+    if (!this.state.products || !this.state.categories) {
       return <div>Loading</div>
     }
 
     return (
-      <div  className="container">
-        {this.renderProducts()}
+      <div className="container">
+        <div>
+          {this.renderCategories()}
+        </div>
+        <div>
+          {this.renderProducts()}
+        </div>
+
       </div>
     )
 
