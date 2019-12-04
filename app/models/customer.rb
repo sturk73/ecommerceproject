7904975458ab3a8878ae69392a0c467bce20255a
Sean Turk <seanturk1@gmail.com>
2019-12-04 11:05:59 -0600

    add validations to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,9 @@ class Customer < ApplicationRecord
   belongs_to :province
   has_secure_password
   has_many :orders
+
+  validates :email, :password_digest, :first_name, :last_name, :phone, :address,
+            :city, :province_id, presence: true
+  validates :email, uniqueness: true
+  validates :phone, numericality: { only_integer: true }
 end
