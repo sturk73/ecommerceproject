1bebf29a9954b57f363a397590d1a1cd02e9ac38
Sean Turk <seanturk1@gmail.com>
2019-12-04 11:06:17 -0600

    add validations to province model

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Province < ApplicationRecord
+  validates :name, :abbrivation, :pst, presence: true
+  validates :name, :abbrivation, uniqueness: true
+  validates :pst, numericality: { only_integer: true }
 end
