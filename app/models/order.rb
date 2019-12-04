a6e3bd3382358e79123834431e9fa88f35c896af
Sean Turk <seanturk1@gmail.com>
2019-12-04 11:06:25 -0600

    add validations to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ApplicationRecord
   has_many :order_products
   has_many :products, through: :order_products
   belongs_to :customers
+
+  validates :subtotal, :tax, :total, :order_date, :customer_id, presence: true
+  validates :subtotal, :tax, :total, numericality: { only_integer: false }
+
 end
